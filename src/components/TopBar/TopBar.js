548661cb70ffc250d8aab97752cd59a111f63326
cnova <caiocnova1@gmail.com>
2019-12-10 13:21:51 -0300

    Add the topbar

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from '../../logo.svg';
-import classes from './TopBar.module.css';
+import classes from './TopBar.css';
 
 const topBar = () => { 
     return (
         <header className={classes.topHead}>
             <img src={logo} alt="logo" className={classes.logo}/>
-            <span>React To Do</span>
+            <span className={classes.topName}>React To Do</span>
         </header>
     );
 };
